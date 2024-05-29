5a6286fc0547e9abf34f2b1d4aede7b7557bd84d
Joooook <zhou39512@gmail.com>
2024-05-29 19:42:03 +0800

    init

--- src/components/QueryTable.vue
+++ src/components/QueryTable.vue
@@ -4,21 +4,34 @@
 </script>
 
 <template>
-  <el-row :gutter="20">
-    <el-col :span="16"><div class="grid-content ep-bg-purple" />1231</el-col>
-    <el-col :span="8"><div class="grid-content ep-bg-purple" />123213</el-col>
-  </el-row>
-  <el-row :gutter="20">
-    <el-col :span="8"><div class="grid-content ep-bg-purple" />123</el-col>
-    <el-col :span="8"><div class="grid-content ep-bg-purple" />123</el-col>
-    <el-col :span="4"><div class="grid-content ep-bg-purple" />123</el-col>
-    <el-col :span="4"><div class="grid-content ep-bg-purple" />123</el-col>
-  </el-row>
-  <el-row :gutter="20">
-    <el-col :span="4"><div class="grid-content ep-bg-purple" />123</el-col>
-    <el-col :span="16"><div class="grid-content ep-bg-purple" />123</el-col>
-    <el-col :span="4"><div class="grid-content ep-bg-purple" />123</el-col>
-  </el-row>
+  <div class="common-layout">
+    <el-container>
+      <el-aside>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          :collapse="isCollapse"
+          @open="handleOpen"
+          @close="handleClose"
+        >
+          <el-menu-item index="1">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>Navigator Two</template>
+          </el-menu-item>
+          <el-menu-item index="2" disabled>
+            <el-icon><document /></el-icon>
+            <template #title>Navigator Three</template>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <el-icon><setting /></el-icon>
+            <template #title>Navigator Four</template>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+
 </template>
 
 <style>
