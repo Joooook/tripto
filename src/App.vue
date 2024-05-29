98f47359c899a1333977249cb870c94a57529cff
Joooook <zhou39512@gmail.com>
2024-05-29 17:05:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script >
+
+import {defineComponent} from "vue";
+import QueryTable from "@/components/QueryTable.vue";
+
+export default defineComponent({
+  components: {QueryTable}
+})
+</script>
+
+<template>
+  <QueryTable/>
+</template>
+
+<style >
+</style>
